1be1410e797e77f8a3a45f50d0adde8303098234
isacmorandin <isacmorandin@gmail.com>
2015-04-04 20:32:30 -0300

    Initial Commit

---
+++ controlefuncionarios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  controlefuncionarios
+//
+//  Created by Isac Morandin on 04/04/15.
+//  Copyright (c) 2015 Curso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ controlefuncionarios/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  controlefuncionarios
+//
+//  Created by Isac Morandin on 04/04/15.
+//  Copyright (c) 2015 Curso. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
